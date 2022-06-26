267c6032334cc75eb6f65e3bc9fc4f014934df4a
wuzhongwei <94050803@qq.com>
2022-06-26 23:17:25 +0800

    打包测试

--- packages/wui-vue/docs/vite.config.js
+++ packages/wui-vue/docs/vite.config.js
@@ -8,4 +8,11 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [vueJsx()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        charset: false // vite build 打包时出现 note: This rule cannot come before a “@charset“ rule
+      }
+    }
+  }
 });
